b37f204062af60fba5b97abe92b803bfafa1c547
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2023-08-01 13:57:35 -0500

    App router setup

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ErrorPages() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
---
+++ src/app/providers.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children, theme }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
